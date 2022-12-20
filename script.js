8c5840f802c346dc68145a51aa87343a2ecf3d66
Igor Borisovskiy <58004557+borisovskiy968@users.noreply.github.com>
2022-12-20 21:02:32 +0100

    Extend project on arbitrary size of grid
    Add a button to prompt the user about the number of squares per side for
    the new grid. Create a function which prompts the user and checks the
    input in order to generate the new grid. Develop code for 16x16 into a
    function that generates grid of squares.

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-const container = document.querySelector('.container');
-container.style.gridAutoColumns = 'minmax(auto, 40px)';
-container.style.gridAutoRows = '40px';
+//Set up initial grid of squares
+let squaresPerSide = 16;
+createGridOfSquares(squaresPerSide);
 
-for (let i = 1; i < 17; i++) {
-  for (let j = 1; j < 17; j++) {
-    const square = document.createElement('div');
-    square.style.gridRow = `${i} / ${i + 1}`;
-    square.style.gridColumn = `${j} / ${j + 1}`;
-    //class 'off lit' is equal to 'lit' because of rule order
-    square.classList.add('off');
-    square.addEventListener('mouseenter', e => e.target.classList.add('lit'));
-    container.appendChild(square);
+function createGridOfSquares(squaresPerSide) {
+  const container = document.querySelector('.container');
+  container.replaceChildren();
+  container.style.gridAutoColumns = `minmax(auto, ${640 / squaresPerSide}px)`;
+  container.style.gridAutoRows = `${640 / squaresPerSide}px`;
+
+  for (let i = 1; i < squaresPerSide + 1; i++) {
+    for (let j = 1; j < squaresPerSide + 1; j++) {
+      const square = document.createElement('div');
+      square.style.gridRow = `${i} / ${i + 1}`;
+      square.style.gridColumn = `${j} / ${j + 1}`;
+      //class 'off lit' is equal to 'lit' because of rule order
+      square.classList.add('off');
+      square.addEventListener('mouseenter', e => e.target.classList.add('lit'));
+      container.appendChild(square);
+    }
+  }
+}
+
+const btnSquaresPerSide = document.querySelector('.btn-squares-per-side');
+btnSquaresPerSide.addEventListener('click', setSquaresPerSide);
+
+function setSquaresPerSide(e) {
+  //As prompt returns null or string, parseInt parses the string and returns
+  //NaN or 1st integer in it (and returns NaN if null given)
+  squaresPerSide =
+    parseInt(prompt(
+      'Please enter the number of squares per side (from 1 to 100):', 16));
+  if (!isNaN(squaresPerSide)) {
+    createGridOfSquares(squaresPerSide);
   }
 }
