8c421eb3de841cf0d63e7e2507bec7404019812a
Igor Borisovskiy <58004557+borisovskiy968@users.noreply.github.com>
2022-12-20 21:25:29 +0100

    Fix comments

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function createGridOfSquares(squaresPerSide) {
       const square = document.createElement('div');
       square.style.gridRow = `${i} / ${i + 1}`;
       square.style.gridColumn = `${j} / ${j + 1}`;
-      //class 'off lit' is equal to 'lit' because of rule order
+      //Class 'lit' overrides 'off' because it is defined later
       square.classList.add('off');
       square.addEventListener('mouseenter', e => e.target.classList.add('lit'));
       container.appendChild(square);
